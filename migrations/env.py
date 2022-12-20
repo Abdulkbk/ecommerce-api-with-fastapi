4a092e829c72084deb1e45f7f9710953859d5a87
Abdulkbk <abdoollahikbk@gmail.com>
2022-12-20 16:42:18 +0100

    db migaration for user and product

--- migrations/env.py
+++ migrations/env.py
@@ -10,6 +10,7 @@ from sqlmodel import SQLModel
 from alembic import context
 
 from authentication.models import User
+from features.product.models import Product
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -24,7 +25,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = SQLModel.metadata
+target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
